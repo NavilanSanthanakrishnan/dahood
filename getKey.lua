07a3ae4f4c4131f892f933db0046742236ef8dcd
NavNav <143132458+NavilanSanthanakrishnan@users.noreply.github.com>
2024-04-18 14:48:32 -0700

    Create getKey.lua

---
+++ getKey.lua
@@ -0,0 +1,9 @@
+-- GetKeyScript
+
+-- Function to generate and return the key
+local function GetKey()
+    return "5B6D3F7H" -- Return the generated key
+end
+
+-- Return the function to be called from the other script
+return GetKey
