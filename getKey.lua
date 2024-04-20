b40d076af3f19fbffe9020f3e0f7b9a1c79f8a97
NavNav <143132458+NavilanSanthanakrishnan@users.noreply.github.com>
2024-04-20 14:30:56 -0700

    Update getKey.lua

--- getKey.lua
+++ getKey.lua
@@ -2,7 +2,7 @@
 
 -- Function to generate and return the key
 local function GetKey()
-    return "5B6D3F7H" -- Return the generated key
+    return "AD817ZC" -- Return the generated key
 end
 
 -- Return the function to be called from the other script
